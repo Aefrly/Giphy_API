2f7e0fa9e6333272894d1b0f5b291dfd03a1213a
Aefrly <fraley.allison@ic.edu>
2025-11-02 13:35:11 -0600

    Just reset everything back to the beginning.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 console.log("script.js loaded");
 
-let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=h0ZNqgdqFrKxlgItueQRwqslfTq0lSEU&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=h0ZNqgdqFrKxlgItueQRwqslfTq0lSEU&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+async function getGif() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  const fact = data.facts[0];
+  
+  // Update the DOM with the dog fact
+  const output = document.getElementById("dog-fact-output"); 
+  output.textContent = fact;
+  }
+
+const button = document.getElementById('fetch-gif-btn');
+button.addEventListener('click', function() {
+    getGif();
+});
